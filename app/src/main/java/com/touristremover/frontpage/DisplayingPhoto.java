35cf036c812dc50648895e87c2691969e24e1cfc
schwa <schwarzem@berkeley.edu>
2019-10-26 08:51:08 -0700

    python chao stuff setup and built actually

---
+++ app/src/main/java/com/touristremover/frontpage/DisplayingPhoto.java
@@ -0,0 +1,14 @@
+package com.touristremover.frontpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayingPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaying_photo);
+    }
+}
